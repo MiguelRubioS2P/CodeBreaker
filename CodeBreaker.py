5cfcdce23ec14a988b08774b0b1b49f58e24d106
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-01-19 17:45:48 +0100

    terminado, pero tiene un bug

--- CodeBreaker.py
+++ CodeBreaker.py
@@ -1,3 +1,5 @@
 import utilidades.utilidades as utility
+import utilidades.JuegoPalabras as codeBreaker
 
 # utility.game()
+codeBreaker.game()
--- utilidades/JuegoPalabras.py
+++ utilidades/JuegoPalabras.py
@@ -1,9 +1,49 @@
 import random
 
 def palabraAleatoria():
-    listaPalabras = ["Joselito","Miguel","Marta","Jorge","MJ"]
+    """
+    En este metodo tenemos una lista de valores, y generando un numero aleatorio devolvemos un
+    valor de esa lista
+    :return:
+    """
+    listaPalabras = ["cinco","migue","marta","jorge","yosua"]
+    numero = random.randint(0, len(listaPalabras)-1)
+    return listaPalabras[numero]
 
-listaPalabras = ["Joselito","Miguel","Marta","Jorge","MJ"]
-numero = random.randint(0,len(listaPalabras))
-print numero
-print listaPalabras[numero]
+
+
+def compararPalabras(palabraIntroducida,palabraGenerada):
+    auxiliar = ""
+    if(len(palabraIntroducida) > 5):
+        return "Tiene mas de 5 caracteres"
+    if(len(palabraIntroducida)<5):
+        return "Tiene menos de 5 caracteres"
+    if(palabraIntroducida.lower() == palabraGenerada):
+        return "Acertaste"
+    else:
+        if(palabraIntroducida[0].lower() == palabraGenerada[0]):
+            auxiliar += palabraIntroducida[0].lower()
+        if(palabraIntroducida[1].lower() == palabraGenerada[1]):
+            auxiliar += palabraIntroducida[1].lower()
+        if(palabraIntroducida[2].lower() == palabraGenerada[2]):
+            auxiliar += palabraIntroducida[2].lower()
+        if(palabraIntroducida[3].lower == palabraGenerada[3]):
+            auxiliar += palabraIntroducida[3].lower()
+        if(palabraIntroducida[4].lower() == palabraGenerada[4]):
+            auxiliar += palabraIntroducida[4].lower()
+        return auxiliar
+
+
+def game():
+    acertado = True
+    palabraAleatorio = palabraAleatoria()
+    
+    while(acertado):
+        print("Introduce una palabra de 5 letras")
+        palabra = raw_input()
+        resultado = compararPalabras(palabra,palabraAleatorio)
+        if(resultado == "Acertaste"):
+            acertado = False
+        print(resultado)
+
+game()
